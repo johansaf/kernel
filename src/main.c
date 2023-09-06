6cea805537fbfecb995d8edda413fd3f0e01ec0e
Johan Saf <johan@pwd.re>
2023-09-06 13:33:51 +0200

    Add function forward declarations
    clang, due to the -pedantic flag, compains otherwise since functions
    without forward declarations is deprecated.

--- src/main.c
+++ src/main.c
@@ -3,13 +3,16 @@ static const int VIDEO_MAX_ROWS = 25;
 
 unsigned short* video_memory = (unsigned short*)0xb8000;
 
-void clear_screen() {
+void clear_screen(void);
+void main(void);
+
+void clear_screen(void) {
   for(int i = 0; i <= VIDEO_MAX_COLS*VIDEO_MAX_ROWS; ++i){
     video_memory[i] = (unsigned short)0x0000;
   }
 }
 
-void main() {
+void main(void) {
   clear_screen();
   while(1){}
 }
